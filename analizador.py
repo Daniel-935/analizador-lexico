8d4c9c5ccddec4a25cf26f6b48c31044b2bff10c
Daniel Bustos Ruiz <76013631+Daniel-935@users.noreply.github.com>
2022-02-04 15:24:18 -0600

    Analizador Actualizado
    El analizador ahora reconoce los numeros enteros, reales e identificadores

--- analizador.py
+++ analizador.py
@@ -1,7 +1,4 @@
 
-import re
-
-
 class analizador:
 
     def __init__(self, e):
@@ -9,194 +6,62 @@ class analizador:
         self.entrada = str(e)
         #Separa la entrada por espacios en una lista
         self.splitEntrada = self.entrada.split(" ")
+        #Declara el estado inicial en 0
+        self.estado = 0
 
-    def printCase(self, num):
+    def returnTipo(self):
 
-        if(num==0):
+        if self.estado == 0:
 
-            return "Identificador"
-        elif(num==1):
+            return "Error"
+
+        elif self.estado == 1:
 
             return "Entero"
-        elif(num==2):
+
+        elif self.estado == 2:
+
+            return "Error"
+
+        elif self.estado == 3:
 
             return "Real"
-        elif(num==3):
 
-            return "Cadena"
-        elif(num==5):
+        elif self.estado == 4:
 
-            return "OpSuma"
-        elif(num==6):
+            return "Identificador"
 
-            return "OpMul"
-        elif(num==7):
+    def evaluaElemento(self, cadena):
 
-            return "OpRelac"
-        elif(num==8):
+        for i in cadena:
 
-            return "OpOR"
-        elif(num==9):
+            #Se mantiene en estado 1 es ENTERO
+            if i.isnumeric() and self.estado==0:
 
-            return "OpAND"
-        elif(num==10):
+                self.estado = 1
 
-            return "OpNOT"
-        elif(num==11):
+            #Si tiene un punto y esta en el estado 1 pasa al segundo estado
+            elif i == "." and self.estado==1:
 
-            return "OpIgualdad"
-        elif(num==12):
+                self.estado = 2
 
-            return ";"
-        elif(num==13):
+            #Si esta en el estado 2 y tiene numero es REAL
+            elif self.estado==2 and i.isnumeric:
 
-            return ","
-        elif(num==14):
+                    self.estado = 3
+                
+            #Si empieza con letra y esta en el estado 0 es IDENTIFICADOR
+            elif i.isnumeric()==False and self.estado==0:
 
-            return "("
-        elif(num==15):
-
-            return ")"
-        elif(num==16):
-
-            return "{"
-        elif(num==17):
-
-            return "}"
-        elif(num==18):
-
-            return "="
-        elif(num==19):
-
-            return "Palabra Reservada 'if'"
-        elif(num==20):
-
-            return "Palabra Reservada 'while'"
-        elif(num==21):
-
-            return "Palabra Reservada 'return'"
-        elif(num==22):
-
-            return "Palabra Reservdada 'else'"
-
-        elif(num==23):
-
-            return "$"
-        elif(num==24):
-
-            return "Palabra Reservada 'int'"
-        elif(num==25):
-
-            return "Palabra Reservada 'float'"
-        elif(num==26):
-
-            return "Palabra Reservada 'void'"
-
-    def isString(self, e):
-
-        number = False
-        letter = False
-
-        for i in e:
-
-            if(i.isnumeric()):
-
-                number = True
-            else:
-
-                letter = True
-        
-        if(number == letter):
-
-            return False
-        
-        return True
-        
-
-    def returnCase(self, entrada):
-
-        if(self.isString(entrada)==False):
-
-            return 0
-        elif(entrada.isnumeric()):
-
-            return 1
-        elif(entrada.find(".")!=-1):
-
-            return 2
-        elif(entrada == "+" or entrada=="-"):
-
-            return 5
-        elif(entrada=="*" or entrada=="/"):
-
-            return 6
-        elif(entrada=="<" or entrada==">" or entrada=="<=" or entrada==">="):
-
-            return 7
-        elif(entrada=="||"):
-
-            return 8
-        elif(entrada=="&&"):
-
-            return 9
-        elif(entrada=="!"):
-
-            return 10
-        elif(entrada=="==" or entrada=="!="):
-
-            return 11
-        elif(entrada==";"):
-
-            return 12
-        elif(entrada==","):
-
-            return 13
-        elif(entrada=="("):
-
-            return 14
-        elif(entrada==")"):
-
-            return 15
-        elif(entrada=="{"):
-
-            return 16
-        elif(entrada=="}"):
-
-            return 17
-        elif(entrada=="="):
-
-            return 18
-        elif(entrada=="if"):
-
-            return 19
-        elif(entrada=="while"):
-
-            return 20
-        elif(entrada=="return"):
-
-            return 21
-        elif(entrada=="else"):
-
-            return 22
-        elif(entrada=="$"):
-
-            return 23
-        elif(entrada=="int"):
-
-            return 24
-        elif(entrada=="float"):
-
-            return 25
-        elif(entrada=="void"):
-
-            return 26
-        else:
-
-            return 3
+                    self.estado = 4
 
     def iniciarAnalizador(self):
    
         for i in self.splitEntrada:
 
-            print(i + "   " + self.printCase(self.returnCase(i))) 
-    
+            #Reinicia el automata
+            self.estado=0
+
+            self.evaluaElemento(i)
+
+            print(i + "\t" + self.returnTipo())    
