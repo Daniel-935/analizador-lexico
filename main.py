07816dfbde9dd0f6f9289eef3c6c43c1baf8a91b
Daniel Bustos Ruiz <76013631+Daniel-935@users.noreply.github.com>
2022-02-26 15:45:01 -0600

    Actualizacion al main
    Ahora el programa puede leer archivos de Excel

--- main.py
+++ main.py
@@ -15,19 +15,3 @@ loc = ("Prueba.xlsx")
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
 
-"""print(sheet.cell_value(0,0))
-print(sheet.cell_value(1,0))
-print(type(sheet.cell_value(0,0)))
-print(type(sheet.cell_value(1,0)))
-print(int(sheet.cell_value(2,0)))
-print(type(sheet.cell_value(2,0)))"""
-
-if sheet.cell_value(2,1) == "":
-
-    print("Esta vacio")
-
-else:
-
-    print("No esta vacio")
-    print(sheet.cell_value(2,1))
-
