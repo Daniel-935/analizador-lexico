cc76cd66dfa9a1bde1aa740745f9840c0bafd5c8
Daniel Bustos Ruiz <76013631+Daniel-935@users.noreply.github.com>
2022-02-26 15:47:44 -0600

    Actualizado el main
    Se borraron las lineas de codigo de una practica futura

--- Etapa_3_Sintactico/main.py
+++ Etapa_3_Sintactico/main.py
@@ -1,33 +1,11 @@
-import xlrd
+
 from analizador import analizador
 from stack import stack
 from sintactico import sintactico
 
 analizador = sintactico()
 
-"""print("Primer Ejercicio: ")
+print("Primer Ejercicio: ")
 analizador.ejercicio_1("hola + mundo")
 print("\nSegundo Ejercicio: ")
-analizador.ejercicio_2("a + b + c + d + e + f")"""
-
-loc = ("Prueba.xlsx")
-
-wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(0)
-
-"""print(sheet.cell_value(0,0))
-print(sheet.cell_value(1,0))
-print(type(sheet.cell_value(0,0)))
-print(type(sheet.cell_value(1,0)))
-print(int(sheet.cell_value(2,0)))
-print(type(sheet.cell_value(2,0)))"""
-
-if sheet.cell_value(2,1) == "":
-
-    print("Esta vacio")
-
-else:
-
-    print("No esta vacio")
-    print(sheet.cell_value(2,1))
-
+analizador.ejercicio_2("a + b + c + d + e + f")
