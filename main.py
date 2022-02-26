671cbdc7a6d93adfbf9a87be2a3d920c948f3924
Daniel Bustos Ruiz <76013631+Daniel-935@users.noreply.github.com>
2022-02-26 16:30:17 -0600

    Actualizado el main.py
    Ahora el programa puede leer archivos

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import xlrd
+from io import open
 from lexico import analizador
 from stack import stack
 from sintactico import sintactico
 
 analizador = sintactico()
 
+file = open("file.txt","r")
+fullString = file.readlines()
 
+for i in fullString:
+
+    print(i)
 
 """loc = ("Prueba.xlsx")
 
