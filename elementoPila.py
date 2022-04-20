3066f5f3f744cfe051ebc20d8ca4a2e34fa3f2fc
Daniel Bustos Ruiz <daniel.bustos4669@alumnos.udg.mx>
2022-04-20 15:24:55 -0500

    Credo el archivo para la clase arbolSintactico

--- elementoPila.py
+++ elementoPila.py
@@ -1,9 +1,13 @@
 
 class elementoPila:
 
-    def __init__(self,v):
+    def __init__(self,v,rNombre,id):
 
         self.valor = v
+        self.nombreRegla = rNombre
+        '''ID PARA IDENTIFICAR EL ELEMENTO'''
+        '''1 = terminal, 2 = noTerminal, 3 = Estado'''
+        self.id = id
 
     def printValor(self):
 
